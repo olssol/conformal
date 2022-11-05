b449e4c6db453b927e32af67a69c89ba9c5cd461
CG Wang <olssol.inc@gmail.com>
2022-11-05 14:27:17 -0400

    init

---
+++ R/cfm_package.R
@@ -0,0 +1,27 @@
+#' The 'conformal' package.
+#'
+#' @docType package
+#' @name    conformal-package
+#' @aliases conformal
+#' # @useDynLib conformal, .registration = TRUE
+#'
+#' @import methods
+#' @import stats
+#' @import ggplot2
+#'
+#' @importFrom grDevices colors
+#' @importFrom graphics axis box legend lines par plot points text arrows grid
+#'     rect
+#' @importFrom parallel detectCores
+#' @importFrom utils as.roman
+#' @importFrom dplyr %>% group_by_ group_by summarize mutate count mutate_if
+#'     rename filter select arrange ungroup n distinct left_join if_else rowwise
+#' @importFrom tidyr gather
+#' @importFrom data.table rbindlist
+#' @importFrom survival Surv survfit coxph survdiff
+#' @importFrom rpact getDesignGroupSequential
+#' @importFrom mvtnorm rmvnorm pmvnorm
+#' @importFrom survminer ggsurvplot
+#'
+#'
+NULL
